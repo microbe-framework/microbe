a9e4c2e768303a1db0a67a8b091cdbe1c7c893ff
microbe-framework <36560493+microbe-framework@users.noreply.github.com>
2018-02-23 04:38:48 +0000

    Add files via upload

---
+++ views/blocks/pages/articles/2.inc.php
@@ -0,0 +1,6 @@
+<div class="microbe-article">
+  <h2>Article #2</h2>
+  <p><a href="<?=$this->getUrl('./articles');?>">Back to articles</a></p>
+  <p>Lorem ipsum</p>
+  <p><a href="<?=$this->getUrl('./articles');?>">Back to articles</a></p>
+</div>
