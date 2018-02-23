342669facc6fc78b8cb1d5d5367ceb43d573bb02
microbe-framework <36560493+microbe-framework@users.noreply.github.com>
2018-02-23 04:41:57 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+/*******************************************************************************
+ *   Project: Microbe PHP Framework
+ *   Version: 0.1.0
+ *    Module: index.php
+ *     Class:
+ *     About: Application entry point
+ *     Begin: 2017/05/01
+ *   Current: 2018/02/22
+ *    Author: Microbe PHP Framework author <microbe-framework@protonmail.com>
+ * Copyright: Microbe PHP Framework author <microbe-framework@protonmail.com>
+ *   License: MIT license 
+ *    Source: https://github.com/microbe-framework/0.1/
+ ******************************************************************************/
+
+/******************************************************************************
+ *            This file is part of the Microbe PHP Framework.                 *
+ *                                                                            *
+ *         Copyright (c) 2017-2018 Microbe PHP Framework author               *
+ *                  <microbe-framework@protonmail.com>                        *
+ *                                                                            *
+ *            For the full copyright and license information,                 *
+ *                     please view the LICENSE file                           *
+ *              that was distributed with this source code.                   *
+ ******************************************************************************/
+
+/******************************************************************************/
+// Error handling
+
+ini_set('display_startup_errors',1);
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+
+/******************************************************************************/
+// Require
+
+//require_once('./library/load.inc.php');
+//require_once('./classes/load.inc.php');
+require_once('./application/load.inc.php');
+
+/******************************************************************************/
+// Application
+
+Application::execute();
+
+/******************************************************************************/
