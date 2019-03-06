d52e27c44f84a37bf02259afb96744e8be6a0a44
Alex <36560493+microbe-framework@users.noreply.github.com>
2019-03-06 02:59:48 -0500

    Add files via upload

--- application/load.inc.php
+++ application/load.inc.php
@@ -1,12 +1,12 @@
 <?php
 /*******************************************************************************
  *   Project: Microbe PHP Framework
- *   Version: 0.1.1
+ *   Version: 0.1.0
  *    Module: loader.inc.php
  *     Class: 
  *     About: application loader sample
  *     Begin: 2017/05/01
- *   Current: 2018/03/02
+ *   Current: 2018/02/22
  *    Author: Microbe PHP Framework author <microbe-framework@protonmail.com>
  * Copyright: Microbe PHP Framework author <microbe-framework@protonmail.com>
  *   License: MIT license
@@ -26,32 +26,37 @@
 
 /******************************************************************************/
 
-// Path to root folder
+// README: Place Your relative path from application root to loader here!
+//$dir = rtrim(__DIR__, '/application');
+//$dir = '/var/www/microbe.lan/public';
 $dir = substr(__DIR__, 0, -1 * strlen('/application'));
+
+//echo __DIR__;
+//echo $dir;
+//exit();
  
 // Microbe PHP Library
 require_once($dir.'/framework/library/Base.class.php');
-require_once($dir.'/framework/library/Path.class.php');
-require_once($dir.'/framework/library/Url.class.php');
-require_once($dir.'/framework/library/Collection.class.php');
-require_once($dir.'/framework/library/Params.class.php');
-require_once($dir.'/framework/library/Debug.class.php');
-require_once($dir.'/framework/library/Http.class.php');
+require_once($dir.'/framework/library/Path.class.php'); // ???
+require_once($dir.'/framework/library/Url.class.php'); // ???
+require_once($dir.'/framework/library/Collection.class.php'); // ???
+require_once($dir.'/framework/library/Params.class.php'); // ???
+require_once($dir.'/framework/library/Debug.class.php'); // ???
+require_once($dir.'/framework/library/Http.class.php'); // ???
 
 // Microbe PHP Framework
-require_once($dir.'/framework/classes/Config.class.php');
-require_once($dir.'/framework/classes/Registry.class.php');
-require_once($dir.'/framework/classes/Globals.class.php');
-require_once($dir.'/framework/classes/Vars.class.php');
+require_once($dir.'/framework/classes/Config.class.php'); // ???
+//require_once($dir.'/framework/classes/Registry.class.php');
+//require_once($dir.'/framework/classes/Globals.class.php');
 require_once($dir.'/framework/classes/Router.class.php');
-require_once($dir.'/framework/classes/RouterEx.class.php');
+require_once($dir.'/framework/classes/RouterEx.class.php'); // ???
 require_once($dir.'/framework/classes/View.class.php');
 require_once($dir.'/framework/classes/Controller.class.php');
 require_once($dir.'/framework/classes/Application.class.php');
 
 // Application
 require_once('AppRouter.class.php');
-require_once('AppView.class.php');
+//require_once('AppView.class.php');
 require_once('AppController.class.php');
 
 /******************************************************************************/
