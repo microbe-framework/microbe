2808e7914ddb9ed1b2d175a6be93b0fb03e38e80
Alex <36560493+microbe-framework@users.noreply.github.com>
2019-03-17 08:09:55 -0500

    Add files via upload

---
+++ application/models/AppModel.class.php
@@ -0,0 +1,35 @@
+<?php
+/*******************************************************************************
+ *   Project: Microbe PHP Framework
+ *   Version: 0.1.1
+ *    Module: AppModel.class.php
+ *     Class: AppModel
+ *     About: Application data model
+ *     Begin: 2019/03/11
+ *   Current: 2019/03/16
+ *    Author: Microbe PHP Framework author <microbe-framework@protonmail.com>
+ * Copyright: Microbe PHP Framework author <microbe-framework@protonmail.com>
+ *   License: MIT license
+ *    Source: https://github.com/microbe-framework/microbe/
+ ******************************************************************************/
+
+/******************************************************************************
+ *            This file is part of the Microbe PHP Framework.                 *
+ *                                                                            *
+ *         Copyright (c) 2017-2019 Microbe PHP Framework author               *
+ *                  <microbe-framework@protonmail.com>                        *
+ *                                                                            *
+ *            For the full copyright and license information,                 *
+ *                     please view the LICENSE file                           *
+ *              that was distributed with this source code.                   *
+ ******************************************************************************/
+ 
+class AppModel extends Microbe\Model
+{
+    /**************************************************************************/
+    // Place Your code here
+
+    /**************************************************************************/
+}
+
+/*******************************************************************************/
