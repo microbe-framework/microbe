e89025fc93f8d46100641efb91fe4f4e44d82021
Alex <36560493+microbe-framework@users.noreply.github.com>
2019-04-08 05:18:39 -0500

    Add files via upload

--- application/blocks/banner.inc.php
+++ application/blocks/banner.inc.php
@@ -1,1 +1,5 @@
 
+<div class="app-banner">
+  <h1><?=$this->getConfigValue('microbe.caption')?></h1>
+  <h3>Version <?=$this->getConfigValue('microbe.version')?> (<?=$this->getConfigValue('microbe.edition')?>)</h3>
+</div>
