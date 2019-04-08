d8035e2ab630b8b05ed95fc11b4e434044b22c4f
Alex <36560493+microbe-framework@users.noreply.github.com>
2019-04-08 05:33:21 -0500

    Add files via upload

---
+++ application/views/blocks/begin.inc.php
@@ -0,0 +1,24 @@
+
+<div class="app-content">
+  <div class="microbe-row" style="background-color1: #f00;">
+    <div class="microbe-col-3" style="background-color1: #0ff;">
+      <div class="app-menu">
+        <h4>Microbe</h4>
+        <ul>
+          <li><a href="<?=$this->getUrl('./about');?>">About</a></li>
+          <li><a href="<?=$this->getUrl('./changes');?>">Changes</a></li>
+          <li><a href="<?=$this->getUrl('./license');?>">License</a></li>
+        </ul>
+        <h4>Handbook</h4>
+        <ul>
+          <li><a href="<?=$this->getUrl('./handbook');?>">Handbook</a></li>
+          <li><a href="<?=$this->getUrl('./handbook/configuration');?>">Configuration</a></li>
+          <li><a href="<?=$this->getUrl('./handbook/routing');?>">Routing</a></li>
+          <li><a href="<?=$this->getUrl('./handbook/controllers');?>">Controllers</a></li>
+          <li><a href="<?=$this->getUrl('./handbook/models');?>">Models</a></li>
+          <li><a href="<?=$this->getUrl('./handbook/views');?>">Views</a></li>
+          <li><a href="<?=$this->getUrl('./handbook/classes');?>">Add classes</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="microbe-col-6" style="background-color1: #ff0;">
---
+++ application/views/blocks/end.inc.php
@@ -0,0 +1,24 @@
+
+    </div>
+  <div class="microbe-col-3">
+<!-- Not for production { -->
+    <div class="app-menu" style="padding-left: 40px;">
+      <h4>Articles</h4>
+      <ul>
+        <li><a href="<?=$this->getUrl('./articles/1');?>">#1: Server variables</a></li>
+        <li><a href="<?=$this->getUrl('./articles/2');?>">#2: Database t_Users</a></li>
+        <li><a href="<?=$this->getUrl('./articles/3');?>">#3: Variables</a></li>
+        <li><a href="<?=$this->getUrl('./articles/4');?>">#4: Database t_News</a></li>
+        <li><a href="<?=$this->getUrl('./articles/5');?>">#5: Auth</a></li>
+        <li><a href="<?=$this->getUrl('./layout');?>">Layout</a></li>
+        <li><a href="<?=$this->getUrl('./template');?>">Template</a></li>
+        <li><a href="<?=$this->getUrl('./redirect');?>">Redirect</a></li>
+        <li><a href="http://192.168.1.2/koshkinkot.ru/web/">koshkinkot.ru [192.168.1.2]</a></li>
+        <li><a href="http://koshkinkot.ru/">koshkinkot.ru</a></li>
+        <li><a href="<?=$this->getUrl('./exit');?>">Exit</a></li>
+      </ul>
+    </div>
+<!-- } Not for production -->
+  </div>
+  </div>
+</div>
