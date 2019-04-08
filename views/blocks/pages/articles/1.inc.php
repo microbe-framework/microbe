208f1fef4ac150ee3386ade538a3d7e52762e73f
Alex <36560493+microbe-framework@users.noreply.github.com>
2019-04-08 04:36:01 -0500

    Delete 1.inc.php

--- views/blocks/pages/articles/1.inc.php
+++
@@ -1,6 +0,0 @@
-<div class="microbe-article">
-  <h2>Article #1</h2>
-  <p><a href="<?=$this->getUrl('./articles');?>">Back to articles</a></p>
-  <p>Lorem ipsum</p>
-  <p><a href="<?=$this->getUrl('./articles');?>">Back to articles</a></p>
-</div>
