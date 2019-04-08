7210baf76816a487499827572ef79aaac1fd3c34
Alex <36560493+microbe-framework@users.noreply.github.com>
2019-04-08 05:27:10 -0500

    Delete banner.inc.php

--- application/blocks/banner.inc.php
+++
@@ -1,5 +0,0 @@
-
-<div class="app-banner">
-  <h1><?=$this->getConfigValue('microbe.caption')?></h1>
-  <h3>Version <?=$this->getConfigValue('microbe.version')?> (<?=$this->getConfigValue('microbe.edition')?>)</h3>
-</div>
