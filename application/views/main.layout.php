dbc644e2d771e9595e46ace089ff76d45ba66c6c
Alex <36560493+microbe-framework@users.noreply.github.com>
2019-05-13 07:53:47 -0500

    Add files via upload

--- application/views/main.layout.php
+++ application/views/main.layout.php
@@ -1,8 +1,8 @@
 <?php
 
-$this->block('blocks/header',   $_);
-$this->block('blocks/banner',   $_);
+$this->block('blocks/head',     $_);
 $this->block('blocks/nav',      $_);
+$this->block('blocks/banner',   $_);
 $this->block('blocks/begin',    $_);
 $this->block('contents/'.$page, $_);
 $this->block('blocks/end',      $_);
