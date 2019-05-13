6f0a627646b47719ec9e35a2feb050a08889d383
Alex <36560493+microbe-framework@users.noreply.github.com>
2019-05-13 07:51:27 -0500

    Update end.inc.php

--- application/views/blocks/end.inc.php
+++ application/views/blocks/end.inc.php
@@ -1,25 +1,5 @@
     </div>
   <div class="microbe-col-3">
-<!-- Not for production { -->
-    <div class="app-menu" style="padding-left: 40px;">
-      <h4>Articles</h4>
-      <ul>
-        <li><a href="<?=$app->getUrl('articles/1');?>">#1: Server variables</a></li>
-        <li><a href="<?=$app->getUrl('articles/2');?>">#2: Database t_Users</a></li>
-        <li><a href="<?=$app->getUrl('articles/3');?>">#3: Variables</a></li>
-        <li><a href="<?=$app->getUrl('articles/4');?>">#4: Database t_News</a></li>
-        <li><a href="<?=$app->getUrl('articles/5');?>">#5: Auth</a></li>
-        <li><a href="<?=$app->getUrl('articles/7');?>">#7: Hydration</a></li>
-        <li><a href="<?=$app->getUrl('articles/8');?>">#8: Admin</a></li>
-        <li><a href="<?=$app->getCmsUrl();?>">CMS</a></li>
-        <li><a href="<?=$app->getUrl('layout');?>">Layout</a></li>
-        <li><a href="<?=$app->getUrl('template');?>">Template</a></li>
-        <li><a href="<?=$app->getUrl('redirect');?>">Redirect</a></li>
-        <li><a href="http://koshkinkot.local/">koshkinkot.local [192.168.1.2]</a></li>
-        <li><a href="<?=$app->getUrl('exit');?>">Exit</a></li>
-      </ul>
-    </div>
-<!-- } Not for production -->
   </div>
   </div>
 </div>
